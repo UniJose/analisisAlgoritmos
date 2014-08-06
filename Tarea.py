d7c933471f9531a81087cf4a47405bc09a159ae3
Josalvarado <joseenriquealvarado@gmail.com>
2014-08-05 23:42:09 -0600

    Tarea #1
    Tarea de análisis de algorimos San José

--- Tarea.py
+++ Tarea.py
@@ -1,3 +1,50 @@
+import time
+import random
+#Tarea 1 Análisis de algoritmos
+#Jose Enrique Alvarado Chaves 201129079
+
+def listRandomGenerator(largo):
+   numberList = []
+   if(largo>1):
+       for i in range(1,largo):
+           number = int(random.uniform(0,500))
+           numberList.append(number)
+   return numberList
+
+def listGeneratorDec(largo):
+   numberList = []
+   if(largo>1):
+       cont=largo
+       while(cont>0):
+           numberList.append(cont)
+           cont=cont-1
+   return numberList
+
+def listGeneratorAc(largo):
+   numberList = []
+   if(largo>1):
+       cont=1
+       while(cont<largo+1):
+           numberList.append(cont)
+           cont=cont+1
+   return numberList
+
+#Esta función fue tomada de internet, para medir el tiempo de un algoritmo.
+#URL: http://elviajedelnavegante.blogspot.com/2010/06/calcular-tiempos-de-ejecucion-en-python.html
+def cronometro(funcion):
+   def funcion_a_ejecutar(*argumentos):
+       # Tiempo de inicio de ejecución.
+       inicio = time.time()
+       # Lanzamos función a ejecutar.
+       ret = funcion(*argumentos)
+       # Tiempo de fin de ejecución.
+       fin = time.time()
+       # Tiempo de ejecución.
+       tiempo_total = fin - inicio
+       # Devolvemos el tiempo de ejecución.
+       return tiempo_total
+   # Devolvemos la función que se ejecuta.
+   return funcion_a_ejecutar
 
 def bubbleSort(pList):
     num = len(pList)
@@ -12,26 +59,64 @@ def bubbleSort(pList):
             Counter= Counter+1
         lenCounter=lenCounter+1
 
-def selectionSort(pList):    
-    for actualPosition in range( len(pList) ):
-        minimumNumber = actualPosition
-        for auxiliarPosition in range(actualPosition+1, len(pList) ):
-            if list[auxiliarPosition] < list[minimumNumber]:
-                minimumNumber = auxiliarPosition
-        temporal = list[minimumNumber]
-        list[minimumNumber] = list[actualPosition]
-        list[actualPosition] = temporal
+def selectionSort(pList):
+     for actualPosition in range(0, len(pList)):
+         minimum = actualPosition
+         for auxiliarPosition in range(actualPosition+1, len(pList)):
+             if pList[auxiliarPosition] < pList[minimum]:
+                 minimum = auxiliarPosition
+         temp = pList[actualPosition]
+         pList[actualPosition] = pList[minimum]
+         pList[minimum] = temp
 
-numberList = [10,9,8,7,6,5,4,3,2,1]
+#Esta funcción se encarga de dar un conjunto de tiempos conforme la lista crece en tamaño
+def getTimerOfSort():
+    counter=5000
+    print("Calculo Bubble Sort con :")
+    while(counter<35000):
+        numberList=listRandomGenerator(counter)
+        print(counter)
+        print(cronometro(bubbleSort)(numberList))
+        counter=counter+5000
+    counter=5000    
+    print("Calculo Selection Sort con :")
+    while(counter<35000):
+        numberList=listRandomGenerator(counter)
+        print(counter)
+        print(cronometro(selectionSort)(numberList))
+        counter=counter+5000
+    counter=5000  
+    print("Calculo Bubble Sort con lista ordenada ascendentemente:")
+    while(counter<35000):
+        numberList=listGeneratorAc(counter)
+        print(counter)
+        print(cronometro(bubbleSort)(numberList))
+        counter=counter+5000
+    counter=5000    
+    print("Calculo Selection Sort con lista ordenada ascendentemente:")
+    while(counter<35000):
+        numberList=listGeneratorAc(counter)
+        print(counter)
+        print(cronometro(selectionSort)(numberList))
+        counter=counter+5000
+    counter=5000       
+    print("Calculo Bubble Sort con lista ordenada descendente:")
+    while(counter<35000):
+        numberList=listGeneratorDec(counter)
+        print(counter)
+        print(cronometro(bubbleSort)(numberList))
+        counter=counter+5000
+    counter=5000       
+    print("Calculo Selection Sort con lista ordenada descendente:")
+    while(counter<35000):
+        numberList=listGeneratorDec(counter)
+        print(counter)
+        print(cronometro(selectionSort)(numberList))
+        counter=counter+5000
 
-print ("Desordenada por BubbleSort:")
-print (numberList)
-#bubbleSort(numberList)
-selectionSort(numberList)
-print ("Ordenada por BubbleSort:")
-print (numberList)
 
-
+getTimerOfSort()
+print("Programa finalizado...")
 
 
         
